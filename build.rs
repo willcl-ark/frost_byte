01aa4b56af409220c9582b12501c01b211f73787
willcl-ark <will@256k1.dev>
2024-04-23 14:06:33 +0100

    move proto to schema

--- build.rs
+++ build.rs
@@ -1,25 +1,25 @@
 fn main() {
     println!("running build script");
     capnpc::CompilerCommand::new()
-        .src_prefix("proto")
-        .file("proto/chain.capnp")
-        .file("proto/common.capnp")
-        .file("proto/echo.capnp")
-        .file("proto/init.capnp")
-        .file("proto/handler.capnp")
-        .file("proto/node.capnp")
-        .file("proto/proxy.capnp")
-        .file("proto/wallet.capnp")
-        .output_path("src/proto")
+        .src_prefix("schema")
+        .file("schema/chain.capnp")
+        .file("schema/common.capnp")
+        .file("schema/echo.capnp")
+        .file("schema/init.capnp")
+        .file("schema/handler.capnp")
+        .file("schema/node.capnp")
+        .file("schema/proxy.capnp")
+        .file("schema/wallet.capnp")
+        .output_path("src")
         .run()
         .expect("schema compiler command failed to run");
 
-    println!("cargo:rerun-if-changed=proto/chain.capnp");
-    println!("cargo:rerun-if-changed=proto/common.capnp");
-    println!("cargo:rerun-if-changed=proto/echo.capnp");
-    println!("cargo:rerun-if-changed=proto/init.capnp");
-    println!("cargo:rerun-if-changed=proto/handler.capnp");
-    println!("cargo:rerun-if-changed=proto/node.capnp");
-    println!("cargo:rerun-if-changed=proto/proxy.capnp");
-    println!("cargo:rerun-if-changed=proto/wallet.capnp");
+    println!("cargo:rerun-if-changed=schema/chain.capnp");
+    println!("cargo:rerun-if-changed=schema/common.capnp");
+    println!("cargo:rerun-if-changed=schema/echo.capnp");
+    println!("cargo:rerun-if-changed=schema/init.capnp");
+    println!("cargo:rerun-if-changed=schema/handler.capnp");
+    println!("cargo:rerun-if-changed=schema/node.capnp");
+    println!("cargo:rerun-if-changed=schema/proxy.capnp");
+    println!("cargo:rerun-if-changed=schema/wallet.capnp");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,31 @@
 mod chain_capnp {
-    include!("proto/chain_capnp.rs");
+    include!("chain_capnp.rs");
 }
 
 mod common_capnp {
-    include!("proto/common_capnp.rs");
+    include!("common_capnp.rs");
 }
 
 mod echo_capnp {
-    include!("proto/echo_capnp.rs");
+    include!("echo_capnp.rs");
 }
 
 mod init_capnp {
-    include!("proto/init_capnp.rs");
+    include!("init_capnp.rs");
 }
 
 mod handler_capnp {
-    include!("proto/handler_capnp.rs");
+    include!("handler_capnp.rs");
 }
 
 mod node_capnp {
-    include!("proto/node_capnp.rs");
+    include!("node_capnp.rs");
 }
 
 mod proxy_capnp {
-    include!("proto/proxy_capnp.rs");
+    include!("proxy_capnp.rs");
 }
 
 mod wallet_capnp {
-    include!("proto/wallet_capnp.rs");
+    include!("wallet_capnp.rs");
 }
