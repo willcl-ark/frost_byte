23e41ba1ee6c450a21746534d942dacb70397f6d
willcl-ark <will@256k1.dev>
2024-04-24 15:30:56 +0100

    make modules pub

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,31 @@
-mod chain_capnp {
+pub mod chain_capnp {
     include!("chain_capnp.rs");
 }
 
-mod common_capnp {
+pub mod common_capnp {
     include!("common_capnp.rs");
 }
 
-mod echo_capnp {
+pub mod echo_capnp {
     include!("echo_capnp.rs");
 }
 
-mod init_capnp {
+pub mod init_capnp {
     include!("init_capnp.rs");
 }
 
-mod handler_capnp {
+pub mod handler_capnp {
     include!("handler_capnp.rs");
 }
 
-mod node_capnp {
+pub mod node_capnp {
     include!("node_capnp.rs");
 }
 
-mod proxy_capnp {
+pub mod proxy_capnp {
     include!("proxy_capnp.rs");
 }
 
-mod wallet_capnp {
+pub mod wallet_capnp {
     include!("wallet_capnp.rs");
 }
