c36125b2c5249f2d92b6f47dbbad95e9b705fe61
josibake <josibake@protonmail.com>
2024-04-24 16:44:52 +0200

    barely working example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    match start_bitcoin_node() {
+        Ok(_) => println!("Bitcoin node started successfully."),
+        Err(e) => eprintln!("Failed to start bitcoin node: {}", e),
+    }
+}
+
+fn start_bitcoin_node() -> std::io::Result<()> {
+    let mut child = Command::new("/root/multiprocess/src/bitcoin-node")
+        .arg("-ipcbind=unix://root/.bitcoin/sockets/node.sock")
+        .spawn()  // Spawns the process
+        .expect("failed to start bitcoin-node process");
+    let _result = child.wait()?;
+    Ok(())
 }
