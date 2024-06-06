824ad25dc38a6633348232dec14f521c0569a0fc
josibake <josibake@protonmail.com>
2024-06-06 16:06:17 +0200

    wip: try to add progress_bar
    some boilerplate, does not compile. next step is sending
    a get progress request and translating the response

--- src/gui.rs
+++ src/gui.rs
@@ -1,18 +1,40 @@
 use crate::spawner::LocalSpawner;
 use iced::widget::button::State;
-use iced::widget::{button, column};
+use iced::widget::{button, column, progress_bar};
 use iced::{executor, Application, Command, Element, Theme};
 use tokio::sync::mpsc;
 
 #[derive(Debug, Clone)]
+pub enum Progress {
+    Started,
+    Advanced(f32),
+    Finished,
+    Error,
+}
+
+pub enum State {
+    Idle,
+    Syncing { progress: f32 },
+    Finished,
+    Errored,
+}
+
+#[derive(Debug, Clone)]
 pub enum WalletMessage {
     CreateNewWallet,
+    SyncProgressed(Progress),
+}
+
+#[derive(Debug)]
+struct SyncProgressed {
+    state: State,
 }
 
 pub struct App {
     create_wallet_button: State,
     spawner: LocalSpawner,
     tx: mpsc::UnboundedSender<WalletMessage>,
+    state: State,
 }
 
 impl Application for App {
@@ -27,6 +49,7 @@ impl Application for App {
                 create_wallet_button: State::new(),
                 spawner: flags.0,
                 tx: flags.1,
+                state: State::Idle,
             },
             Command::none(),
         )
@@ -45,7 +68,32 @@ impl Application for App {
         Command::none()
     }
 
+    pub fn subscription(&self) -> Subscription<Self::Message> {
+        match self.state {
+            State::Syncing { .. } => {
+                GetProgress()
+                    .map(Message::DownloadProgressed)
+            }
+            _ => Subscription::none(),
+        }
+    }
+
     fn view(&self) -> Element<Self::Message> {
-        column![button("Create new wallet").on_press(WalletMessage::CreateNewWallet),].into()
+        let current_progress = match &self.state {
+            State::Idle { .. } => 0.0,
+            State::Syncing { progress } => *progress,
+            State::Finished { .. } => 100.0,
+            State::Errored { .. } => 0.0,
+        };
+
+        let progress_bar = progress_bar(0.0..=100.0, current_progress);
+        let control = button("Create new wallet").on_press(WalletMessage::CreateNewWallet)
+        Column::new()
+            .spacing(10)
+            .padding(10)
+            .align_items(Alignment::Center)
+            .push(progress_bar)
+            .push(control)
+            .into()
     }
 }
