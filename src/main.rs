c1444b7be3e01bb032fcc1a345219851e5f646fd
willcl-ark <will@256k1.dev>
2024-08-07 22:11:07 +0100

    autospawn

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,97 @@
 use anyhow::Result;
+use clap::Parser;
 use frost_byte::gui::{App, WalletMessage};
 use frost_byte::spawner::LocalSpawner;
 use frost_byte::tasks::Task;
+use nix::sys::signal::{kill, Signal};
+use nix::unistd::Pid;
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use std::env;
+use std::panic;
 use std::path::PathBuf;
-use std::str::FromStr;
+use std::process;
+use std::process::{Child, Command, Stdio};
+use std::sync::{Arc, Mutex};
 use tokio::sync::mpsc;
+use tokio::time::{sleep, Duration};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to IPC socket
+    #[arg(long, conflicts_with = "spawn")]
+    socket: Option<PathBuf>,
+
+    /// Auto spawn a bitcoin-node binary at this path
+    #[arg(long, conflicts_with = "socket")]
+    spawn: Option<String>,
+}
 
 fn main() -> Result<()> {
+    let spawned_process: Arc<Mutex<Option<Child>>> = Arc::new(Mutex::new(None));
+
+    // Set up a custom panic hook to alert that bitcoin-node will be left running
+    panic::set_hook(Box::new(move |panic_info| {
+        eprintln!("Panic occurred: {:?}", panic_info);
+        eprintln!("`bitcoin-node` process will be left running and should be terminated manually");
+        eprintln!("hint: use `bitcoin-cli [-network] stop` or `pkill bitcoin-node`");
+        process::exit(1);
+    }));
+
     let rt = tokio::runtime::Builder::new_multi_thread()
         .worker_threads(4)
         .enable_all()
         .build()
         .unwrap();
 
-    rt.block_on(async {
-        let args: Vec<String> = std::env::args().collect();
-        if args.len() != 2 {
-            println!("Usage:\n\t{} [path]", args[0]);
-            return Ok(());
-        }
-        let path = PathBuf::from_str(&args[1]).unwrap();
+    let result = rt.block_on(async {
+        let args = Args::parse();
+
+        let socket_path = match (&args.socket, &args.spawn) {
+            (Some(socket), None) => socket.clone(),
+            (None, Some(autospawn_cmd)) => {
+                let temp_path = random_temp_path();
+
+                let child = Command::new(autospawn_cmd)
+                    .arg(format!("-ipcbind=unix://{}", temp_path.to_str().unwrap()))
+                    .arg("-debug=ipc")
+                    .arg("-regtest")
+                    .arg("-daemon=0")
+                    .stdout(Stdio::null())
+                    .stderr(Stdio::null())
+                    .spawn()?;
+
+                println!("Spawned process: {:?}", child);
+                println!("Temporary file created at: {}", temp_path.display());
+
+                // Save the child process
+                *spawned_process.lock().unwrap() = Some(child);
+
+                // Wait for the socket to be created
+                for _ in 0..30 {
+                    // Try for 30 seconds
+                    if temp_path.exists() {
+                        break;
+                    }
+                    sleep(Duration::from_secs(1)).await;
+                }
+                temp_path
+            }
+            (None, None) => {
+                // Neither --socket nor --spawn provided, use default
+                let default_path = default_socket_path();
+                println!("Using default socket path: {}", default_path.display());
+                default_path
+            }
+            _ => unreachable!("Clap arg parsing gone wrong!"),
+        };
 
         let spawner = LocalSpawner::new();
 
         // Setup initial connection
         let (send, response) = tokio::sync::oneshot::channel();
-        spawner.spawn(Task::SetupConnection(path, send));
+        spawner.spawn(Task::SetupConnection(socket_path, send));
         match response.await {
             Ok(Ok(())) => println!("Connection setup successfully"),
             Ok(Err(e)) => println!("Error occurred: {}", e),
@@ -63,5 +129,38 @@ fn main() -> Result<()> {
         .expect("Failed to run app");
 
         Ok(())
-    })
+    });
+
+    // Handle clean exit
+    if let Some(mut child) = spawned_process.lock().unwrap().take() {
+        terminate_process(&mut child);
+    }
+
+    result
+}
+
+fn default_socket_path() -> PathBuf {
+    dirs::home_dir()
+        .unwrap_or_else(|| PathBuf::from("/"))
+        .join(".bitcoin")
+        .join("sockets")
+        .join("node.sock")
+}
+
+fn random_temp_path() -> PathBuf {
+    let mut rng = thread_rng();
+    let random_string: String = (0..10).map(|_| rng.sample(Alphanumeric) as char).collect();
+    env::temp_dir().join(format!("frost_byte_{}", random_string))
+}
+
+fn terminate_process(child: &mut Child) {
+    let pid = Pid::from_raw(child.id() as i32);
+    eprintln!("Attempting graceful shutdown of spawned process...");
+    if let Err(e) = kill(pid, Signal::SIGTERM) {
+        eprintln!("Failed to send SIGTERM: {}", e);
+        eprintln!("Attempting forceful termination...");
+        if let Err(e) = child.kill() {
+            eprintln!("Failed to kill spawned process: {}", e);
+        }
+    }
 }
